ff69adf9c4b45cd3ceaa2537764fd626b2a7a692
SaeedMolaiy <saeedmolaiy.dev@gmail.com>
2025-07-07 15:01:39 +0330

    Initializing project

---
+++ src/Chess/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Chess
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
