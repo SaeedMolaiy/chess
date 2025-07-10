0c6c34a49dd2c4e2de6616159ebd5410d1e61713
SaeedMolaiy <saeedmolaiy.dev@gmail.com>
2025-07-10 19:05:45 +0330

    Added chess board + some basic styles

---
+++ src/Chess/Engine/ChessBoard.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Chess
+{
+    internal class ChessBoard
+    {
+        private const int BoardSize = 8;
+
+        private readonly Grid _chessBoardGrid;
+
+        public ChessBoard(Grid chessBoardGrid)
+        {
+            _chessBoardGrid = chessBoardGrid;
+
+            RenderChessBoard();
+        }
+
+        private void RenderChessBoard()
+        {
+            var whiteSquaresColor = (Color)ColorConverter.ConvertFromString("#EBECD0");
+            var blackSquaresColor = (Color)ColorConverter.ConvertFromString("#779455");
+
+            _chessBoardGrid.RowDefinitions.Clear();
+            _chessBoardGrid.ColumnDefinitions.Clear();
+
+            for (int i = 0; i < BoardSize; i++)
+            {
+                _chessBoardGrid.RowDefinitions.Add(new RowDefinition());
+                _chessBoardGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            for (int row = 0; row < BoardSize; row++)
+            {
+                for (int col = 0; col < BoardSize; col++)
+                {
+                    bool isWhiteSquare = (row + col) % 2 == 0;
+
+                    var rect = new Border
+                    {
+                        Background = new SolidColorBrush(isWhiteSquare ? whiteSquaresColor : blackSquaresColor),
+                        BorderBrush = Brushes.Transparent,
+                        BorderThickness = new Thickness(0)
+                    };
+
+                    Grid.SetRow(rect, row);
+                    Grid.SetColumn(rect, col);
+
+                    _chessBoardGrid.Children.Add(rect);
+                }
+            }
+        }
+    }
+}
--- src/Chess/MainWindow.xaml.cs
+++ src/Chess/MainWindow.xaml.cs
@@ -4,9 +4,13 @@ namespace Chess
 {
     public partial class MainWindow : Window
     {
+        private readonly ChessBoard _board;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _board = new ChessBoard(ChessBoardGrid);
         }
     }
 }
