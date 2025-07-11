6e178fc32d1e472942966bd58a9c774f2981f179
SaeedMolaiy <saeedmolaiy.dev@gmail.com>
2025-07-11 17:13:25 +0330

    Added title bar

--- src/Chess/MainWindow.xaml.cs
+++ src/Chess/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace Chess
 {
@@ -9,8 +10,31 @@ namespace Chess
         public MainWindow()
         {
             InitializeComponent();
+            ConfigureEventHandlers();
 
             _board = new ChessBoard(ChessBoardGrid);
+        }
+
+        private void ConfigureEventHandlers()
+        {
+            CloseButton.Click += CloseButton_Click;
+            MinimizeButton.Click += MinimizeButton_Click;
+            MaximizeButton.Click += MaximizeButton_Click;
+
+            TitleBar.MouseLeftButtonDown += TitleBar_MouseLeftButtonDown;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e) => Close();
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e) =>
+            WindowState = (WindowState == WindowState.Normal) ? WindowState.Maximized : WindowState.Normal;
+
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+                DragMove();
         }
     }
 }
